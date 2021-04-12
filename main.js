b5cd06d21db4a4b5adf80793ee8377b5af87cf7f
saidfernando <saidfer83@gmail.com>
2021-04-12 15:20:19 -0500

    operadores_de_comparacion_31_03_2021

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+let a = 5;
+let b = '5';
+
+console.log(a, typeof(a));
+console.log(b, typeof(b));
+
+console.warn("Menor");
+console.log(a < b); //Operador de menor
+console.warn("Menor o igual");
+console.log(a <= b); //Operador de menor o igual
+console.warn("Mayor");
+console.log(a > b); //Operador de mayor
+console.warn("Mayor o igual");
+console.log(a >= b); //Operador de mayor o igual
+console.warn("Igualdad");
+console.log(a == b); //Operador de igualdad 'Compara es si el dato es igual'
+console.warn("Identico");
+console.log(a === b); //Operador de identico 'Compara si el dato y el tipo de dato es igual e identico'
+console.warn("NO Igualdad");
+console.log(a != b); // Operador de No igual 'Compara si el valor de los dos datos no son igual'
+console.warn("No Identico");
+console.log(a !== b); // Operador de No identico 'Compara si el valor y el tipo de datos no son identicos uno de lostros si los datos son igula compara los tipo de datos'
